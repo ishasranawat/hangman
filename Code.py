f6bc37eb7c4283b80ff9316aa1a3d8c41e3839b7
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-10 15:10:17 +0530

    Code.py

---
+++ Code.py
@@ -0,0 +1,29 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
+placeholder= ""
+word_length= len(chosen_word)
+for position in range(word_length):
+    placeholder+="_"
+
+
+
+display= ""
+for letter in chosen_word:
+    if letter== guess:
+        display+=letter
+    else:
+        display+= "_"
+
+print(display)
