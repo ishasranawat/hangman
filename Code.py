04b3b62d37722f54e4ab875e085214074f0f3568
Isha <158483807+ishasranawat@users.noreply.github.com>
2024-11-11 19:39:42 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,27 +1,56 @@
 import random
-word_list = ["summer", "winter", "monsoon", "spring"]
+
+# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+
+lives = 6
+
+# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-guess = input("Guess a letter: ").lower()
-
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
-
-placeholder= ""
-word_length= len(chosen_word)
+placeholder = ""
+word_length = len(chosen_word)
 for position in range(word_length):
-    placeholder+="_"
+    placeholder += "_"
+print("Word to guess: " + placeholder)
 
-display= ""
-for letter in chosen_word:
-    if letter== guess:
-        display+=letter
-    else:
-        display+= "_"
+game_over = False
+correct_letters = []
 
-print(display)
+while not game_over:
+
+    # TODO-6: - Update the code below to tell the user how many lives they have left.
+    print("****************************<???>/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word:
+        lives -= 1
+
+        if lives == 0:
+            game_over = True
+
+           
+            print(f"***********************YOU LOSE**********************")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    print(stages[lives])
+
